6ec908a8741d459937526a5aa5fd21243fd5f9c8
AlekhyaPathipati <106488346+AlekhyaPathipati@users.noreply.github.com>
2022-06-17 22:10:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import TodoList from './TodoList';
+import './index.css';
+import './JavaScript';
+
+const App = () => {
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const changeHandler = e => {
+    setTask(e.target.value)
+  }
+  const submitHandler = e => {
+    e.preventDefault();
+    const newTodos = [...todos, task];
+    setTodos(newTodos);
+    setTask("");
+  }
+  const deleteHandler = (indexValue) => {
+    const newTodos = todos.filter((todo, index) => index !== indexValue);
+    setTodos(newTodos);
+  }
+  return (
+    <div className='main'>
+      <center>
+              <h4 className="title">Todo Management Application</h4>
+              <form onSubmit={submitHandler}>
+                <input size="30" type="text" name="task" value={task} onChange={changeHandler} /> &nbsp;&nbsp;
+                <input type="submit" value="Add Todo" name="Add Todo" />
+              </form>
+              <TodoList todolist={todos} deleteHandler={deleteHandler} />
+      </center>
+    </div>
+  )
+}
+
+export default App;
